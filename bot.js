af3d72c075309e88b1761c1f10c16f1b84630db2
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2018-12-07 16:15:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -481,23 +481,6 @@ collector7.on('collect', r => {
 
 
 
-client.on('ready', function(){    
-    var ms = 5000 ;    
-    var setGame = ["+help","+inv",`${client.guilds.size} Servers`",By ! ᴸᶜ 𓅓 𝟳𝗠𝗗🌠👑"];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`https://www.twitch.tv/dggamingbot`);    
-}, ms);    
-    
-});
 
 
 client.on('ready', () => {
