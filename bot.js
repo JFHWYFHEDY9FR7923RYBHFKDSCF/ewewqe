33b8ce2a33122657ba17bf4c28d75df124d9ed70
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2018-12-07 16:18:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -485,6 +485,7 @@ collector7.on('collect', r => {
 
 client.on('ready', () => {
 client.user.setStatus("dnd");
+client.user.setGame(`+help | +inv ${client.guilds.size} Servers  `,"https://www.twitch.tv/dggamingbot")
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
   console.log('')
