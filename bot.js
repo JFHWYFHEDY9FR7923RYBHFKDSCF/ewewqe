48c91b47da64b12ad8e76679ad9d619cf63f54b5
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2019-04-20 02:23:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -548,6 +548,28 @@ if (msg.content.startsWith(`+sug`)) {
   }
 })
 
+client.on('message', message => {
+     if(!message.channel.guild) return;
+var prefix = "+";
+                if(message.content.startsWith(prefix + 'bots')) {
+
+    
+    if (message.author.bot) return;
+    let i = 1;
+        const botssize = message.guild.members.filter(m=>m.user.bot).map(m=>`${i++} - <@${m.id}>`);
+          const embed = new Discord.RichEmbed()
+          .setAuthor(message.author.tag, message.author.avatarURL)
+          .setDescription(`**Found ${message.guild.members.filter(m=>m.user.bot).size} bots in this Server**
+${botssize.join('\n')}`)
+.setFooter(client.user.username, client.user.avatarURL)
+.setTimestamp();
+message.channel.send(embed)
+
+}
+
+
+});
+
 
 client.on('message', msg => { 
 if (msg.content.startsWith(`+report`)) {
